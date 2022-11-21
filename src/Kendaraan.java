a8780078a6c5a3e6f04bcb507e2953c8f244020c
RandyTunru <randytunru@gmail.com>
2022-11-21 10:44:22 +0800

    updated Main.java, adding input system

--- src/Kendaraan.java
+++ src/Kendaraan.java
@@ -1,2 +1,4 @@
-package PACKAGE_NAME;public class Kendaraan {
+public class Kendaraan {
+    String jenis;
+    String noPol;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,23 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        int option;
+        Scanner intScanner = new Scanner(System.in);
+        Scanner strScanner = new Scanner(System.in);
+        while (true){
+            System.out.println("1. Parkir masuk");
+            System.out.println("2. Parkir keluar");
+            System.out.print("Pilihan anda : ");
+            int pilihan = intScanner.nextInt();
+            if (pilihan == 1){
+
+            }else if (pilihan == 2){
+
+            }else{
+                System.out.println("Opsi tidak tersedia");
+            }
+
+        }
+    }
 }
