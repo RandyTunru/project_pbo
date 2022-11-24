be0cc5a3d7b16d47ccc883165eabb52a2f1005d9
RandyTunru <randytunru@gmail.com>
2022-11-25 04:16:03 +0800

    finished parkir keluar function, need bug testing

---
+++ src/Info_masuk.java
@@ -0,0 +1,13 @@
+import java.sql.Date;
+import java.sql.Time;
+
+public class Info_masuk {
+    int no_tiket;
+    String noPol;
+    String jenis;
+    Date tggl_masuk;
+    Time jam_masuk;
+    int biaya;
+    Date tggl_keluar;
+    Time jam_keluar;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
+import java.sql.*;
 import java.util.Scanner;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
@@ -38,7 +35,7 @@ public class Main {
         System.out.println("2. Motor");
         System.out.print("Pilihan anda : ");
         int pilihan = intScanner.nextInt();
-        if (pilihan == 1){
+        if (pilihan == 1) {
             newKendaraan.jenis = "Mobil";
         } else if (pilihan == 2) {
             newKendaraan.jenis = "Motor";
@@ -61,6 +58,63 @@ public class Main {
     }
 
     public static void parkirKeluar(){
+        Scanner strScanner = new Scanner(System.in);
+        Connection c = null;
+        Statement stmt = null;
+        Info_masuk infoBaru = new Info_masuk();
 
+        System.out.print("Masukkan nomor polisi : ");
+        infoBaru.noPol = strScanner.nextLine();
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/parkingsystem", "postgres", "@Randy2003");
+            c.setAutoCommit(false);
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(String.format("SELECT NO_TIKET FROM INFO_MASUK WHERE NOPOL = '%s' AND BIAYA IS NULL;", infoBaru.noPol));
+            rs.next();
+            infoBaru.no_tiket = rs.getInt("NO_TIKET");
+            stmt.executeUpdate(String.format("UPDATE INFO_MASUK SET TGGL_KELUAR = CURRENT_DATE, JAM_KELUAR = CURRENT_TIME WHERE NO_TIKET = %d;", infoBaru.no_tiket));
+            rs = stmt.executeQuery(String.format("SELECT * FROM INFO_MASUK WHERE NO_TIKET = %d;", infoBaru.no_tiket));
+            rs.next();
+            infoBaru.jenis = rs.getString("JENIS");
+            infoBaru.tggl_masuk = rs.getDate("TGGL_MASUK");
+            infoBaru.jam_masuk = rs.getTime("JAM_MASUK");
+            infoBaru.tggl_keluar = rs.getDate("TGGL_KELUAR");
+            infoBaru.jam_keluar = rs.getTime("JAM_KELUAR");
+            if (infoBaru.jenis.equals("Motor")){
+                infoBaru.biaya = 3000 + calcTotalWaktu(infoBaru) * 2000;
+            } else {
+                infoBaru.biaya = 4000 + calcTotalWaktu(infoBaru) * 3000;
+            }
+            stmt.executeUpdate(String.format("UPDATE INFO_MASUK SET BIAYA = %d WHERE NO_TIKET = %d;", infoBaru.biaya, infoBaru.no_tiket));
+
+            stmt.close();
+            c.commit();
+            c.close();
+
+            System.out.println(infoBaru.no_tiket);
+            System.out.println(infoBaru.jenis);
+            System.out.println(infoBaru.tggl_masuk);
+            System.out.println(infoBaru.tggl_keluar);
+            System.out.println(infoBaru.jam_masuk);
+            System.out.println(infoBaru.jam_keluar);
+            System.out.println(infoBaru.biaya);
+
+        }catch (Exception e){
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            return;
+        }
+    }
+
+    public static int calcTotalWaktu(Info_masuk dataBaru){
+        int jam_keluar = Integer.parseInt(dataBaru.jam_keluar.toString().substring(0,2));
+        int jam_masuk = Integer.parseInt(dataBaru.jam_masuk.toString().substring(0,2));
+
+        if ((jam_keluar - jam_masuk) > 2){
+            return 2;
+        }
+
+        return jam_keluar - jam_masuk;
     }
 }
